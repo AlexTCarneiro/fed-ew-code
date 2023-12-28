90583cc44f420b184cb219987d3c332acdbaa1c2
Alexandre de Tarcio Carneiro <installer4us@gmail.com>
2023-12-28 13:30:41 -0300

    Update latest-3-posts.php
    This is a simple PHP code snippet related to front-end development in WordPress. In a scenario where I'm customizing a WordPress theme and want to display the latest three posts in a specific category on the front end.
    This code uses the class WP_Query {} to retrieve and display the latest three posts from the specified category. It includes the post title, a link to the post, and a brief excerpt from each post.
    
    *More comments added

--- latest-3-posts.php
+++ latest-3-posts.php
@@ -1,14 +1,23 @@
 <?php
-// Display the latest three posts from a specific category
+/**
+ * File Name: Latest 3 Posts
+ * Description: A WordPress Mod to display the latest three posts from a specific category.
+ * Author: Alex C
+ * Version: 1.0.1
+ */
 
+// Define query parameters for retrieving the latest three posts from a specific category
 $args = array(
     'category_name' => 'the_category_slug', // Replace with the actual category slug
     'posts_per_page' => 3,
 );
 
+// Create a new instance of WP_Query using the defined parameters
 $latest_posts_query = new WP_Query($args);
 
+// Check if there are posts available based on the query
 if ($latest_posts_query->have_posts()) :
+    // Loop through each post in the query result
     while ($latest_posts_query->have_posts()) : $latest_posts_query->the_post();
 ?>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -16,8 +25,10 @@ if ($latest_posts_query->have_posts()) :
 <?php
     endwhile;
 else :
+    // Display a message if no posts are found
     echo 'No posts found';
 endif;
 
+// Reset post data to restore the global post object
 wp_reset_postdata();
 ?>
